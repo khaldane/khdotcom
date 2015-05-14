2553de1a07630be3ed757c6b8a331475e0722dcd
khaldane <k.haldane19@gmail.com>
2015-05-14 00:12:55 -0400

    website

---
+++ app/views/index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width" />
+    <meta name="author" content="Katherine Haldane" />
+    <meta name="robots" content="Index" />
+    <meta name="keywords" content="web, develop, khaldane, application" />
+    <meta name="description" content="Simply web." />
+
+    <title>khaldane</title>
+
+    <link rel="stylesheet" href="style/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="components-font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style/style.css">
+
+    <script src="javascript/jquery-1.11.2.min.js"></script>
+    <script src="style/bootstrap/js/bootstrap.min.js"></script>
+    <script src="javascript/app.js"></script>
+</head>
+<body>
+    <header class="navbar navbar-static-top bs-docs-nav" id="top" role="banner">
+        <nav class="navbar navbar-default">
+          <div class="container">
+             <div class="container-fluid">
+                <div class="navbar-header">
+                  <a class="navbar-brand" href="#"> KH </a>
+              </div>
+                <ul class="nav nav-pills navbar-right nav-end">
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Blog</i></a></li>
+                </ul>
+             </div>
+          </div>
+        </nav>
+    </header>
+    <main id="content">
+        <div id="header-wrap">
+          <!-- Header image/content here-->
+        </div>
+        <div id="myWork"> My Work</div>
+        <div id="myOtherWork">My Other Work</div>
+        <div id="contact">Contact Me</div>
+    </main>
+    <footer id="footer-wrap">
+      <div id="footer-content"></div>
+      <div id="footer"></div>
+<!--        <nav class="navbar navbar-default">
+          <div class="container">
+             <div class="container-fluid">
+                <div class="navbar-header">
+                  <a class="navbar-brand" href="#"> KH </a>
+              </div>
+                <ul class="nav nav-pills navbar-right nav-end">
+                    <li><a href="#"><i class="fa fa-facebook-square facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin-square linkedin"></i></a></li>
+                    <li><a href="#"><i class="fa fa-github github" ></i></a></li>
+                    <li><a href="#"><i class="fa fa-envelope-o email"></i></a></li>
+                </ul>
+             </div>
+          </div>
+        </nav> -->
+    </footer>
+</body>
+</html>
