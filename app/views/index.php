aefa5cbb370ed91e734cbe4da4300040c4927c3b
Katherine Haldane <katherine.haldane@hotmail.com>
2015-08-29 00:04:31 -0400

    Landing

--- app/views/index.php
+++ app/views/index.php
@@ -9,8 +9,9 @@
     <meta name="description" content="Simply web." />
 
     <title>khaldane</title>
-    
+    <link rel="shortcut icon" type="image/x-icon" href="/images/khLogoBrowser.png" />
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700, 100' rel='stylesheet' type='text/css'>
     
     <link rel="stylesheet" href="style/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="components-font-awesome/css/font-awesome.min.css">
@@ -21,48 +22,65 @@
     <script src="javascript/app.js"></script>
 </head>
 <body>
-    <div id="wrapper">
-    <div class="navbar navbar-static-top bs-docs-nav" id="top" role="banner">
+    <div class="navbar navbar-fixed-top bs-docs-nav" id="top" role="banner">
         <nav class="navbar navbar-default">
-          <div class="container">
-             <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="#"> <img id="logo" src="/images/khlogo.png" alt="Katherine Haldane Logo" /> </a>
-                </div>
-                <ul class="nav nav-pills navbar-left nav-start">
-                    <li><a href="#" class="nav">PORTFOLIO</a></li>
-                    <li><a href="#" class="nav">GAMING</a></li>
-                    <li><a href="#" class="nav">TUTORIALS</i></a></li>
-                    <li><a href="#" class="nav">CONTACT</i></a></li>
-                </ul>
-             </div>
-          </div>
+         <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#"> <img id="logo" src="/images/khlogo.png" alt="Katherine Haldane Logo" /> </a>
+            </div>
+            <ul class="nav nav-pills navbar-left nav-start">
+                <li><a href="#" class="nav">PORTFOLIO</a></li>
+                <li><a href="#" class="nav">GAMING</a></li>
+                <li><a href="#" class="nav">TUTORIALS</i></a></li>
+                <li><a href="#" class="nav">CONTACT</i></a></li>
+            </ul>
+         </div>
         </nav>
     </div>
-    <header class="main-header">
-        
-    </header>
+
     <main id="content">
-        
+        <div class="content-left">
+            <div class="about-wrap">
+                <img class="about-img" src="/images/khaldane.png" alt="Katherine Haldane" />
+                <h2>Katherine Haldane</h2>
+                <p class="about">
+                    Welcome! My name is Katherine and I craft websites and mobile apps.
+                    <br>
+                    Want to go on a hunt? Learn more about me by finding the 5 easter eggs hidden within my site!
+                </p>
+                <div class="download-resume">
+                    <p> <img class="download-img" src="images/download.png"/> DOWNLOAD RESUME</p>
+                </div>
+            </div>
+        </div>
+        <div class="content-right">
+            <div class="featured-product">
+                <div class="featured-overlay"></div>
+                <div class="featured-content">
+                    blah
+                </div>
+            </div>
+        </div>
     </main>
     
-    <footer id="footer-wrap" class="container-fluid">
-        <div class="row">
-            <div class="col-sm-6 footer-social-media">
-                <ul class="nav-footer">
-                    <li><a href="#"><i class="fa fa-facebook facebook "></i></a></li>
-                    <li><a href="#"><i class="fa fa-linkedin-square linkedin"></i></a></li>
-                    <li><a href="#"><i class="fa fa-github github" ></i></a></li>
-                    <li><a href="#"><i class="fa fa-envelope-o email"></i></a></li>
-                </ul>
-            </div>
-            <div class="col-sm-6">
-                <div class="footer-copyright">
-                    © Katherine Haldane 2015
+    <footer id="footer-wrap">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-6 footer-social-media">
+                    <ul class="nav-footer">
+                        <li><a href="#"><i class="fa fa-facebook facebook "></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin-square linkedin"></i></a></li>
+                        <li><a href="#"><i class="fa fa-github github" ></i></a></li>
+                        <li><a href="#"><i class="fa fa-envelope-o email"></i></a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-6">
+                    <div class="footer-copyright">
+                        © Katherine Haldane 2015
+                    </div>
                 </div>
             </div>
         </div>
     </footer>
-    </div>
 </body>
 </html>
