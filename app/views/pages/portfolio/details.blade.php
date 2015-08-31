08ba1c1c8851466d46bae2fdb0b9b323dfb932e7
Katherine Haldane <katherine.haldane@hotmail.com>
2015-08-31 11:05:04 -0400

    Case Study details

--- app/views/pages/portfolio/details.blade.php
+++ app/views/pages/portfolio/details.blade.php
@@ -20,7 +20,37 @@
             </div>
         </div>
         <div class="details-body">
+            <h2 class="details-title">About The Project</h2>
+            <p  class="about details-content">
+                Flannel cardigan health goth plaid. Wayfarers keffiyeh Pitchfork swag. Freegan pop-up squid, retro lumbersexual PBR yr cornhole DIY Banksy. Cold-pressed pour-over single-origin coffee, Truffaut mixtape                           freegan cardigan crucifix actually jean shorts butcher. PBR narwhal Carles kogi, before they sold out whatever 3 wolf moon keffiyeh heirloom fingerstache keytar. Marfa keffiyeh flexitarian, drinking                               vinegar tousled Truffaut meh cronut. Keytar disrupt meditation pug, mumblecore forage crucifix Truffaut four dollar toast hashtag Portland 8-bit.
+                <br>
+                Deep v put a bird on it brunch salvia kale chips 90's DIY tofu, XOXO Neutra butcher blog paleo. Fixie Tumblr artisan, try-hard letterpress mixtape selfies pour-over leggings deep v sriracha. Tote bag                             Portland cold-pressed, lo-fi try-hard retro plaid aesthetic distillery taxidermy XOXO. Hella post-ironic Bushwick chillwave, ugh small batch cardigan direct trade pour-over pork belly quinoa vinyl cold-                           pressed shabby chic synth. Listicle trust fund gastropub, vinyl four dollar toast Truffaut narwhal mustache. Small batch High Life McSweeney's farm-to-table gastropub, cred food truck kitsch church-key                           semiotics. Keffiyeh meggings try-hard, fingerstache quinoa Bushwick trust fund fashion axe.
+                <br>
+                Slow-carb four dollar toast Portland ethical tattooed PBR irony, Odd Future trust fund. XOXO fingerstache small batch, butcher viral PBR whatever. Bicycle rights fashion axe heirloom Marfa iPhone                                 lumbersexual. Semiotics four loko skateboard, before they sold out iPhone banjo disrupt readymade dreamcatcher flannel cronut four dollar toast bitters. Blue Bottle leggings Austin artisan, bicycle rights                         freegan selvage jean shorts keytar sriracha keffiyeh Odd Future food truck. Banjo ennui artisan cred selfies, keytar vinyl chia tattooed banh mi polaroid. Gastropub chia ennui normcore ethical                                     letterpress.
+            </p>
             
+            <!-- image here -->
+            <div col-sm-12>
+                <div class="details-content-img"> </div>
+            </div>
+            <div class="details-features">
+                <h3> Features </h3>
+                <p class="about" >Flannel cardigan health goth plaid. Wayfarers keffiyeh Pitchfork swag.</p>
+                <div class="col-sm-6">
+                    <ul>
+                        <li> feature 1 </li>
+                        <li> feature 2 </li>
+                        <li> feature 3 </li>
+                    </ul>
+                </div>
+                <div class="col-sm-6">
+                    <ul>
+                        <li> feature 1 </li>
+                        <li> feature 2 </li>
+                        <li> feature 3 </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 @stop
